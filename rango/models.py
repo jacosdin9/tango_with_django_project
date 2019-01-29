95fea82ebe766219cde0decb91731bdaef4675d2
Jacob Osdin <2314964o@student.gla.ac.uk>
2019-01-29 19:11:14 +0000

    Chapter 5 without exercise

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self): 
         return self.name
 
@@ -13,4 +16,5 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
 
     def __str__(self): 
-        return self.title
+        return self.title
+
