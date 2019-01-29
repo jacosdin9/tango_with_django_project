648996bb7244cc3a53f4c05a1c6d85f18c10beed
Jacob Osdin <2314964o@student.gla.ac.uk>
2019-01-29 13:42:25 +0000

    Updated

--- rango/views.py
+++ rango/views.py
@@ -5,4 +5,7 @@ def index(request):
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
-
+def about(request):
+    context_dict = {'boldmessage': "Rango says here is the about page."}
+    return render(request, 'rango/about.html', context=context_dict)
+    
